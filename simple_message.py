9c74d4f2e897a3f18a0054ee2683979add28b8cf
Mike Wilson <mwilso17@my.westga.edu>
2021-06-01 09:17:21 -0400

    new file containing printed messages from variable

---
+++ simple_message.py
@@ -0,0 +1,8 @@
+# this program will assign a simple message to a variable then print that
+# message.
+
+message = "This is my message"
+print(message)
+
+message = "I updated the message variable but left the name the same."
+print(message)

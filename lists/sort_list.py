607359435c749c9e2b313e6e5c8616460d30fd84
Mike Wilson <mwilso17@my.westga.edu>
2021-06-05 13:35:59 -0400

    added new list and sorted reverse alphabetically

--- lists/sort_list.py
+++ lists/sort_list.py
@@ -6,3 +6,12 @@ instruments = ['guitar', 'violin', 'bass', 'drums', 'piano']
 
 # prints the origional list
 print(instruments)
+
+# sorts alphabetically
+instruments.sort()
+print(instruments)
+
+# Sorting a new list reverse alphabetically
+vacation_destinations = ['germany', 'france', 'spain', 'russia', 'canada']
+instruments.sort(reverse=True)
+print(vacation_destinations.title())

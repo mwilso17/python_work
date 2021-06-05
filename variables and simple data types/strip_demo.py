588b206386e4e7a8bd72e6b49d914c0c14aca1fd
Mike Wilson <mwilso17@my.westga.edu>
2021-06-05 09:49:14 -0400

    new program demonstrating how to strip whitespace

---
+++ variables and simple data types/strip_demo.py
@@ -0,0 +1,9 @@
+# Mike Wilson 5 June 2021
+# The following program demonstrates how to use various strip methods in Python
+# so that white space can be eliminated
+
+name = ' Mia the ball python '
+
+print(name.lstrip())
+print(name.rstrip())
+print(name.strip())

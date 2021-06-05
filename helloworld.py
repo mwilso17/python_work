dcbc0b74039801bc8680fc1d7bbc55ed863993c9
Mike Wilson <mwilso17@my.westga.edu>
2021-06-05 09:24:03 -0400

    changed variable

--- helloworld.py
+++ helloworld.py
@@ -6,3 +6,7 @@ print("I am doing this to ensure version control works just fine.")
 # below demonstrates how to use a variable in python
 message = "Hello World!"
 print(message)
+
+# below demonstrates how to change a variable after it has been used
+message ="Hello again, world!"
+print(message)

cac4b2860ff43e3710c3afae8b88339d3d7623b6
Mike Wilson <mwilso17@my.westga.edu>
2021-06-05 09:42:15 -0400

    program displaying a famous quote.

---
+++ variables and simple data types/famous_quote.py
@@ -0,0 +1,7 @@
+# Mike Wilson 5 June 2021
+# This program displays a quote by my favorite martial artist
+
+famous_person = 'Bruce Lee'
+quote = 'I fear not the man who has practiced 10,000 kicks once, but the man who has practice 1 kick 10,000 times.'
+message = f'{famous_person} once said, "{quote}"'
+print(message)

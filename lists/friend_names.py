d80b64d1086a202f005fef6005bb175dd3b92049
Mike Wilson <mwilso17@my.westga.edu>
2021-06-05 11:13:07 -0400

    added greeting variable

--- lists/friend_names.py
+++ lists/friend_names.py
@@ -7,3 +7,6 @@ print(f"{friend_names[0].title()} is my friend. He is great!")
 print(f"{friend_names[1].title()} is my friend. She is great!")
 print(f"{friend_names[2].title()} is my friend. He is great!")
 print(f"{friend_names[-1].title()} is my friend. He is terrible!")
+
+greeting = f"{friend_names[0].title()}, I hope you are doing well today."
+print(greeting)

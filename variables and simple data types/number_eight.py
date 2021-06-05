673eb7a88d53e5c6140ccbe7d2c51cbdef5ad172
Mike Wilson <mwilso17@my.westga.edu>
2021-06-05 10:00:27 -0400

    new program: how to use math operations

---
+++ variables and simple data types/number_eight.py
@@ -0,0 +1,8 @@
+# Mike Wilson 5 June 2021
+# This is a program that demonstrates how to use the simple math operations
+# in python
+
+print(5 + 3)
+print(10 - 2)
+print(4 * 2)
+print (16 / 2) # this one will return a float

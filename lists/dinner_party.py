8676f384bfce1ed2c602d22eeac8b0454b5cd697
Mike Wilson <mwilso17@my.westga.edu>
2021-06-05 11:26:17 -0400

    2 variables and a print message

--- lists/dinner_party.py
+++ lists/dinner_party.py
@@ -2,3 +2,7 @@
 # This program demonstrates various ways to manipulate a list and simulates
 # a dinner party experience. A special thanks to all the Greats involved 
 # and a certain Terrible one.
+
+guest_list = ['Catherine', 'Frederick', 'Alexander']
+message = 'would you like to come to a great dinner party?'
+print(f"{guest_list[0].title()}, {message}")

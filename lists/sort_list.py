35281a9911335986dcbd2be5e5c479d65e230096
Mike Wilson <mwilso17@my.westga.edu>
2021-06-05 13:28:44 -0400

    new program for sorting lists

---
+++ lists/sort_list.py
@@ -0,0 +1,8 @@
+# Mike Wilson 5 June 2021
+# This program demonstrates how to sort a few lists alphabetically and
+# reverse alphabetically
+
+instruments = ['guitar', 'violin', 'bass', 'drums', 'piano']
+
+# prints the origional list
+print(instruments)

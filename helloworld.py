d6ae464c3cd2f4b06fdcb83a570e8c41b4ac5e8d
Mike Wilson <mwilso17@my.westga.edu>
2021-06-05 09:22:32 -0400

    added variable to helloworld

--- helloworld.py
+++ helloworld.py
@@ -2,3 +2,7 @@ print("Hello World. This is Python program.")
 
 print("This is something I added after the initial line.")
 print("I am doing this to ensure version control works just fine.")
+
+# below demonstrates how to use a variable in python
+message = "Hello World!"
+print(message)

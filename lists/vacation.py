97cb652ec39508c6e51555360696e8dcc0a4435e
Mike Wilson <mwilso17@my.westga.edu>
2021-06-05 13:47:28 -0400

    initial lines of code to establish and print list

---
+++ lists/vacation.py
@@ -0,0 +1,7 @@
+# Mike Wilson 5 June 2021
+# This program manipulates a list in various ways.
+
+vacation_places = ['peru', 'germany', 'brazil', 'canada', 'iceland']
+
+# printing list in origional order
+print(vacation_places)

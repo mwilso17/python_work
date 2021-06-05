11c8cd583710122ef70f6037ed8f0482aa859ecc
Mike Wilson <mwilso17@my.westga.edu>
2021-06-05 11:23:34 -0400

    new program for a great dinner party

---
+++ lists/dinner_party.py
@@ -0,0 +1,4 @@
+# Mike Wilson 5 June 2021
+# This program demonstrates various ways to manipulate a list and simulates
+# a dinner party experience. A special thanks to all the Greats involved 
+# and a certain Terrible one.

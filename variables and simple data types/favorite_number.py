88888743bf4466c568ca1aa48dca33d0d1f7d7c2
Mike Wilson <mwilso17@my.westga.edu>
2021-06-05 10:03:10 -0400

    new program displays someone's favorite number

---
+++ variables and simple data types/favorite_number.py
@@ -0,0 +1,6 @@
+# Mike Wilson 5 June 2021
+# This program uses a variable to print someone's favorite number.
+
+favorite_number = '42' #can change number to whatever you want.
+print("What is my favorite number?\n")
+print(f"My favorite number is {favorite_number}.")

7f88735e1cabb4deba6a6b99f6cdaf648119bc19
Mike Wilson <mwilso17@my.westga.edu>
2021-06-05 11:27:47 -0400

    added a 4th guest

--- lists/dinner_party.py
+++ lists/dinner_party.py
@@ -3,8 +3,9 @@
 # a dinner party experience. A special thanks to all the Greats involved 
 # and a certain Terrible one.
 
-guest_list = ['Catherine', 'Frederick', 'Alexander']
+guest_list = ['Catherine', 'Frederick', 'Alexander', 'Ivan']
 message = 'would you like to come to a great dinner party?'
 print(f"{guest_list[0].title()}, {message}")
 print(f"{guest_list[1].title()}, {message}")
 print(f"{guest_list[2].title()}, {message}")
+print(f"{guest_list[3].title()}, {message}")

1ebd043c4d9adf2e33f9cfbfab02a83736315051
Mike Wilson <mwilso17@my.westga.edu>
2021-06-05 10:20:15 -0400

    program contains a list and calls on its elements

---
+++ lists/friend_names.py
@@ -0,0 +1,9 @@
+# Mike Wilson 5 June 2021
+# This program stores the name of some people in a list and calls upon them
+# individually
+
+friend_names = ['alexander', 'catherine', 'frederick', 'ivan']
+print(f"{friend_names[0].title()} is my friend. He is great!")
+print(f"{friend_names[1].title()} is my friend. She is great!")
+print(f"{friend_names[2].title()} is my friend. He is great!")
+print(f"{friend_names[-1].title()} is my friend. He is terrible!")

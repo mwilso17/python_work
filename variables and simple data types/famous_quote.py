4762d424e3bef85d7456dcd647ed5f5df22455e5
Mike Wilson <mwilso17@my.westga.edu>
2021-06-05 09:44:41 -0400

    put string in lower case, added .title to message

--- variables and simple data types/famous_quote.py
+++ variables and simple data types/famous_quote.py
@@ -1,7 +1,9 @@
 # Mike Wilson 5 June 2021
 # This program displays a quote by my favorite martial artist
+# There is no need to change message or print. You can change famous_person or 
+# quote, however.
 
-famous_person = 'Bruce Lee'
+famous_person = 'bruce lee'
 quote = 'I fear not the man who has practiced 10,000 kicks once, but the man who has practice 1 kick 10,000 times.'
-message = f'{famous_person} once said, "{quote}"'
+message = f'{famous_person.title()} once said, "{quote}"' 
 print(message)

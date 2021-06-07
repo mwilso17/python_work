463656ae38c81412c736d75fe24ae9c43bacaf3b
Mike Wilson <mwilso17@my.westga.edu>
2021-06-07 08:06:00 -0400

    added another program demonstrating a for loop

---
+++ lists/lists_loops/animals.py
@@ -0,0 +1,9 @@
+# Mike Wilson 7 June 2021
+# This program demonstrates how to use a for loop.
+
+animals = ['dog', 'cat', 'snake', 'ferret']
+
+for animal in animals:
+  print(f"A {animal} would make a great pet!")
+
+print("\nAny of these animals would make a great pet!")

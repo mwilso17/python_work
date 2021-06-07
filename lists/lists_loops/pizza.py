c713dc8c5593c6acc1c701536936323196a53731
Mike Wilson <mwilso17@my.westga.edu>
2021-06-07 07:58:17 -0400

    program to demonstrate a simple for loop

---
+++ lists/lists_loops/pizza.py
@@ -0,0 +1,10 @@
+# Mike Wilson 7 June 2021
+# This program demonstrates how to use a simple 'for' loop with a list.
+# A statement at the end shows how to code outside of a for loop
+
+favorite_pizzas = ['pepperoni', 'deep dish', 'double cheese', 'mushroom']
+
+for pizza in favorite_pizzas:
+  print(f"I really like {pizza} pizza.")
+
+print("\nI really love all types of pizza!")

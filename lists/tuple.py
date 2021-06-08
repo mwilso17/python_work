9073f5bb3279a8887c9001a93f80bfece8a206c6
Mike Wilson <mwilso17@my.westga.edu>
2021-06-08 07:57:11 -0400

    Modified variables of tuple

--- lists/tuple.py
+++ lists/tuple.py
@@ -2,7 +2,15 @@
 # This program shows an example of a tuple. The example is a small restaurant
 # that has a menu of items that should not change.
 
-menu = ('burger', 'fries', 'chicken tenders', 'soda', 'ice cream')
+menu = ('burgers', 'fries', 'chicken tenders', 'soda', 'ice cream')
+
+for item in menu:
+  print(f"This restaurant serves {item}.")
+
+# Although you cannot modify a tuple, you can assign a new value to a 
+# variable within a tuple.
+
+menu = ('burgers', 'chips', 'chicken sandwichs', 'soda', 'ice cream')
 
 for item in menu:
   print(f"This restaurant serves {item}.")

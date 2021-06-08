127eb4f8167199d598b20e479139798aa108ca63
Mike Wilson <mwilso17@my.westga.edu>
2021-06-08 06:32:53 -0400

    program to add all digits, 1 to 1 million

---
+++ lists/lists_loops/sum_of_one_million.py
@@ -0,0 +1,7 @@
+# Mike Wilson 8 June 2021
+# This program shows how to add the sum of numbers in a list
+
+numbers = list(range(1, 1_000_001))
+
+print(min(numbers))
+print(max(numbers)) #These two lines of code make sure that we have the right range

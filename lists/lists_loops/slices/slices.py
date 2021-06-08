68261af707858c0fd849c1337668039608e35d63
Mike Wilson <mwilso17@my.westga.edu>
2021-06-08 07:15:38 -0400

    added list and first slice

--- lists/lists_loops/slices/slices.py
+++ lists/lists_loops/slices/slices.py
@@ -1,3 +1,7 @@
 # Mike Wilson 8 June 2021
 # This prgoram demonstrates how to use slices in Python
 
+german_cities = ['Berlin', 'Munich', 'Oldenburg', 'Hamburg', 'Bremen', 'Ulm']
+
+print('The first three cities on the list are: ')
+print(german_cities[0 : 3])

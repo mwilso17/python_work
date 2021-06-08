7516f1da5254223642f1e4b71521751fd11e8ed4
Mike Wilson <mwilso17@my.westga.edu>
2021-06-08 06:53:25 -0400

    demonstrated refactoring of code

--- lists/lists_loops/cubes.py
+++ lists/lists_loops/cubes.py
@@ -3,8 +3,17 @@
 
 # The following code demonstrates 1 way to write a program that cubes the correct numbers
 cubes = []
+
 for value in range(1, 11):
   cube = value ** 3
   cubes.append(cube)
+
+print(cubes)
+
+# The above code can be refactored to shorten the number of lines used
+cubes = []
+
+for value in range(1, 11):
+  cubes.append(value**3)
 
 print(cubes)

c0bc5729aa0a8fb20d383b91bbb0ab306937b708
Mike Wilson <mwilso17@my.westga.edu>
2021-06-08 07:46:19 -0400

    program demonstrating a tuple

---
+++ lists/tuple.py
@@ -0,0 +1,8 @@
+# Mike Wilson 8 June 2021
+# This program shows an example of a tuple. The example is a small restaurant
+# that has a menu of items that should not change.
+
+menu = ('burger', 'fries', 'chicken tenders', 'soda', 'ice cream')
+
+for item in menu:
+  print(f"This restaurant serves {item}.")

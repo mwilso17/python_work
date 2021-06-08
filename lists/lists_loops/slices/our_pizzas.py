5a4b3b8f2c2fecae8a625a22a98de45daa3ec7e5
Mike Wilson <mwilso17@my.westga.edu>
2021-06-08 07:27:50 -0400

    added initial code

---
+++ lists/lists_loops/slices/our_pizzas.py
@@ -0,0 +1,9 @@
+# Mike Wilson 8 June 2021
+# This program slices from one list and adds it to another, while keeping
+# two seperate lists that operate independantly of one another.
+
+my_favorite_pizzas = ['pepperoni', 'deep dish', 'mushroom']
+your_favorite_pizzas = my_favorite_pizzas[:]
+
+print("My favorite pizzas are: ")
+print(my_favorite_pizzas)

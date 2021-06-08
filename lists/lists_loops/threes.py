f0df86d6d9eb6a970e2e6181e7cf8362f9a6fab8
Mike Wilson <mwilso17@my.westga.edu>
2021-06-08 06:43:58 -0400

    program to list multiples of 3, 3-31

---
+++ lists/lists_loops/threes.py
@@ -0,0 +1,8 @@
+# Mike Wilson 8 June 2021
+# This program makes a list of the multiples of 3 from 3 to 30.
+# Program should print 3, 6, 9, 12, 15, 18, 21, 24, 27, 30.
+
+numbers = list(range(3, 31, 3))
+
+for number in numbers:
+  print(number)

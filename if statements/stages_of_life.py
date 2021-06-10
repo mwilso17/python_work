fd58ad5553645623f37e0d3cc3d53e76869ad8f6
Mike Wilson <mwilso17@my.westga.edu>
2021-06-10 06:11:00 -0400

    additional steps of if-elif-else chain added

--- if statements/stages_of_life.py
+++ if statements/stages_of_life.py
@@ -2,9 +2,17 @@
 # This program uses an if-elif-else chain to print a message based on someone's
 # age.
 
-age = 3
+age = 2  #age can be changed to whatever number is desired. 
 
-if age <= 2:
+if age <= 1:
   print("You are a baby.")
-elif age <= 4:
+elif age <= 3:
   print("You are a toddler.")
+elif age <= 12:
+  print("You are a kid.")
+elif age <= 19:
+  print("You are a teenager.")
+elif age <= 64:
+  print("You are an adult.")
+else:
+  print("You are an elder.")

a1e9634ffcecf256861b92d82156afd3ebe78908
Mike Wilson <mwilso17@my.westga.edu>
2021-06-10 06:42:20 -0400

    for loop with if else statement

--- if statements/hello_admin.py
+++ if statements/hello_admin.py
@@ -1,3 +1,11 @@
 # Mike Wilson 10 June 2021
 # This program loops through a list to print a greeting for each user.
 # There is also a special greeting for the admin.
+
+users = ['admin', 'marshall', 'lilly', 'ted', 'robin']
+
+for name in users:
+  if name == 'admin':
+    print('Hello admin. Would you like to see a status report?')
+  else:
+    print(f"Hello {name.title()}. How are you doing today?")

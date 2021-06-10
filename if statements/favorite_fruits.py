f51e22ed7a421ac2cb25cb7d8367ff29191e54b7
Mike Wilson <mwilso17@my.westga.edu>
2021-06-10 06:21:54 -0400

    code to print  if an item is in list

--- if statements/favorite_fruits.py
+++ if statements/favorite_fruits.py
@@ -2,3 +2,13 @@
 # This program uses a series of 'if' statements to check if certain items
 # are on a list or not.
 
+favorite_fruits = ['apple', 'banana', 'orange']
+
+if 'apple' in favorite_fruits:
+  print("Apples are great!")
+if 'orange' in favorite_fruits:
+  print("Oranges are great!")
+if 'blueberry' in favorite_fruits:
+  print("Blueberries are great!")
+if 'kiwi' in favorite_fruits:
+  print("Kiwis are great!")

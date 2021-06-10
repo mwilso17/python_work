c7fed02d76dc32b8586803bcc7d156c961a5956d
Mike Wilson <mwilso17@my.westga.edu>
2021-06-10 06:06:18 -0400

    added elif statement and test

--- if statements/stages_of_life.py
+++ if statements/stages_of_life.py
@@ -2,7 +2,9 @@
 # This program uses an if-elif-else chain to print a message based on someone's
 # age.
 
-age = 1
+age = 3
 
 if age <= 2:
   print("You are a baby.")
+elif age <= 4:
+  print("You are a toddler.")

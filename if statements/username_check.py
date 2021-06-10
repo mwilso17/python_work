b465d02dfa53c7e448770dbf9071fa0565458021
Mike Wilson <mwilso17@my.westga.edu>
2021-06-10 06:55:06 -0400

    initial for loop

--- if statements/username_check.py
+++ if statements/username_check.py
@@ -4,3 +4,9 @@
 
 current_users = ['mike', 'bob', 'sally', 'bob2', 'jeff']
 new_users = ['bob', 'mike', 'josh', 'ash', 'danny']
+
+for new_user in new_users:
+  if new_user in current_users:
+    print(f"Sorry, but the username: {current_users} is already taken.")
+  else:
+    print(f"{new_user} is available as a username!")

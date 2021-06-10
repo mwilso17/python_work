a58fea294b604d263535b63718bcc3a473749f51
Mike Wilson <mwilso17@my.westga.edu>
2021-06-10 07:28:46 -0400

    for loop with if-elif-else chain added

--- if statements/ordinal_numbers.py
+++ if statements/ordinal_numbers.py
@@ -3,3 +3,13 @@
 # ie: 1 turns to 1st. 2 turns to 2nd, etc.
 
 numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+for number in numbers:
+  if number == 1:
+    print("1st")
+  elif number == 2:
+    print("2nd")
+  elif number == 3:
+    print("3rd")
+  else:
+    print(f"{number}th")

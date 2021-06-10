27a94f373e479835e8d69c84948fdd47363b86c3
Mike Wilson <mwilso17@my.westga.edu>
2021-06-10 06:52:25 -0400

    created current user and new user lists

--- if statements/username_check.py
+++ if statements/username_check.py
@@ -2,3 +2,5 @@
 # This program simulates how a website verifies that every user has a unique
 # username.
 
+current_users = ['mike', 'bob', 'sally', 'bob2', 'jeff']
+new_users = ['bob', 'mike', 'josh', 'ash', 'danny']

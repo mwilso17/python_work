87d07649c696f44e3e89d262e57426e4ab76a0b1
Mike Wilson <mwilso17@my.westga.edu>
2021-06-10 05:59:23 -0400

    if,elif,else chain and comments

--- if statements/alien_colors_1.py
+++ if statements/alien_colors_1.py
@@ -2,7 +2,11 @@
 # This program simulates an alien spaceship being shot down by the player
 # and prints a message giving points based on the color of the spaceship.
 
-alien_color = 'green'
+alien_color = 'red' #this variable can be changed to green, yellow, or another color
 
 if alien_color == 'green':
   print("You earned 5 points!")
+elif alien_color == 'yellow':
+  print("You earned 10 points!")
+else:
+  print("You earned 15 points!")

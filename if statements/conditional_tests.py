b5e165a27127ac04df0819632b5b84386e34bd48
Mike Wilson <mwilso17@my.westga.edu>
2021-06-10 05:44:18 -0400

    added additional tests

--- if statements/conditional_tests.py
+++ if statements/conditional_tests.py
@@ -1,6 +1,18 @@
 # Mike Wilson 10 June 2021
-# This program shows 10 examples of conditional tests.
+# This program shows a few examples of conditional tests.
 
 car = 'honda'
 print("Is car.title() == 'Honda'? I predict True.")
 print(car.title() == 'Honda')
+
+fruit = 'tomato'
+print("Is fruit == 'tomato'? I predict True.")
+print(fruit == 'tomato')
+
+pet = 'cat'
+print("Is pet == 'cat'? I predict True.")
+print(pet == 'cat')
+
+name = 'mike'
+print("Is name == 'Mike'? I predict False!")
+print(name == 'Mike') #this is because Python is case sensitive

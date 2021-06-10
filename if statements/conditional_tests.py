1a4bda48dfbfab83b85f467a44a0ecba6242ac7a
Mike Wilson <mwilso17@my.westga.edu>
2021-06-10 05:39:01 -0400

    program to show conditional tests/ give examples

---
+++ if statements/conditional_tests.py
@@ -0,0 +1,6 @@
+# Mike Wilson 10 June 2021
+# This program shows 10 examples of conditional tests.
+
+car = 'honda'
+print("Is car.title() == 'Honda'? I predict True.")
+print(car.title() == 'Honda')

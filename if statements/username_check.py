0020c41cd45fb11f22dc5ff051c74f807144e15e
Mike Wilson <mwilso17@my.westga.edu>
2021-06-10 07:00:57 -0400

    added case sensitivity functionality

--- if statements/username_check.py
+++ if statements/username_check.py
@@ -2,11 +2,13 @@
 # This program simulates how a website verifies that every user has a unique
 # username.
 
-current_users = ['mike', 'bob', 'sally', 'bob2', 'jeff']
-new_users = ['bob', 'mike', 'josh', 'ash', 'danny']
+current_users = ['mike', 'Bob', 'sally', 'bob2', 'Jeff']
+new_users = ['bob', 'Mike', 'josh', 'ash', 'danny']
+
+current_users_lower = [current_users.lower() for user in current_users]
 
 for new_user in new_users:
-  if new_user in current_users:
+  if new_user.lower() in current_users_lower:
     print(f"Sorry, but the username: '{new_user}' is already taken.")
   else:
     print(f"{new_user} is available as a username!")

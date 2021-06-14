36adfae3b8418964d84d5adc5932022128c68187
Mike Wilson <mwilso17@my.westga.edu>
2021-06-14 06:59:48 -0400

    copied glossary.py and changed comment

---
+++ dictionaries/glossary_2.py
@@ -0,0 +1,26 @@
+# Mike Wilson 14 June 2021
+# This program is a copy of glossary.py but uses a loop and sorts them
+# alphabetically.
+
+glossary = {
+  'boolean': 'an expression that can be evaluated as either true or false.',
+  'string': 'a series of characters.',
+  'dictionary': 'a set of key value pairs.',
+  'function': 'a block of code that performs a task.',
+  'list': 'a collection of items in a certain order.',
+}
+
+word = 'boolean'
+print(f"\n{word.title()}: {glossary[word]}")
+
+word = 'string'
+print(f"\n{word.title()}: {glossary[word]}")
+
+word = 'dictionary'
+print(f"\n{word.title()}: {glossary[word]}")
+
+word = 'function'
+print(f"\n{word.title()}: {glossary[word]}")
+
+word = 'list'
+print(f"\n{word.title()}: {glossary[word]}")

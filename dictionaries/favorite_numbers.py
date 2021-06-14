c17408a4a190fe1abd1724beda1c02cbb0eb6e35
Mike Wilson <mwilso17@my.westga.edu>
2021-06-14 06:15:38 -0400

    made dictionary and key value pairs

--- dictionaries/favorite_numbers.py
+++ dictionaries/favorite_numbers.py
@@ -1,3 +1,10 @@
 # Mike Wilson 14 June 2021
 # This program stores people's favorite numbers and prints the key value pairs
 
+favorite_numbers = {
+  'Mike': 30,
+  'Ash': 42,
+  'John': 120,
+  'Bill': 7,
+  'Dan': 413,
+}

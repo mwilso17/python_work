da35711941ef5054aee1a92e85981c1e98b84dbd
Mike Wilson <mwilso17@my.westga.edu>
2021-06-14 07:48:36 -0400

    made additional dictionaries

--- dictionaries/people.py
+++ dictionaries/people.py
@@ -2,3 +2,26 @@
 # This program creates a dictionary and adds a dictionary to it and uses a 
 # 'for' loop to access the data within.
 
+# Make an empty list to store people in.
+people = []
+
+# Define some people and add them to the list above.
+person = {
+  'name': 'mike',
+  'city': 'atlanta',
+  'age': 30,
+}
+people.append(person)
+
+person = {
+  'name': 'julian',
+  'city': 'boston',
+  'age': 28,
+}
+people.append(person)
+
+person = {
+  'name': 'rob',
+  'city': 'houston',
+  'age': '41'
+}

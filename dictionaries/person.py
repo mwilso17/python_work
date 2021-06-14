a0feb260f951a61b790b1f3bc719d0dbf5fad76c
Mike Wilson <mwilso17@my.westga.edu>
2021-06-14 06:12:05 -0400

    added code to call upon key value pairs

--- dictionaries/person.py
+++ dictionaries/person.py
@@ -5,6 +5,11 @@
 person = {
   'first_name': 'tony',
   'last_name': 'stark',
-  'age': '42',
+  'age': 42,
   'city': 'new york city' 
 }
+
+print(person['first_name'].title())
+print(person['last_name'].title())
+print(person['age'])
+print(person['city'].title())

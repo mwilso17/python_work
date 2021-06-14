78708ee5c3f1fa64be586c2bd4864db2e2a15ef6
Mike Wilson <mwilso17@my.westga.edu>
2021-06-14 08:09:30 -0400

    added dictionaries

--- dictionaries/pets.py
+++ dictionaries/pets.py
@@ -1,3 +1,31 @@
 # Mike Wilson 14 June 2021
 # This program has several dictionaries and prints details of each one using
-# a single 'for' loop.
+# a single 'for' loop when nesting them inside of a list.
+
+# Empty dictionary to store other dictionaries in.
+pets = []
+
+# Dictionaries containing info about people's pets.
+pet = {
+  'name': 'mia',
+  'owner': 'mike',
+  'age': 1,
+  'species': 'ball python',
+}
+pets.append(pet)
+
+pet = {
+  'name': 'spaz',
+  'owner': 'rob',
+  'age': 3,
+  'species': 'cat'
+}
+pets.append(pet)
+
+pet = {
+  'name': 'bobba'
+  'owner': 'alice'
+  'age': 5,
+  'species': 'ferret'
+}
+pets.append(pet)

be1622349cb2aff8edf6c36c3148633f977ca00a
Mike Wilson <mwilso17@my.westga.edu>
2021-06-14 07:13:55 -0400

    added second 'for' loop to print river names

--- dictionaries/rivers.py
+++ dictionaries/rivers.py
@@ -10,3 +10,8 @@ rivers = {
 
 for country, river in rivers.items():
   print(f"\nThe {river.title()} river runs through {country.title()}.")
+
+print("The following rivers can be found in the dictionary:")
+for river in rivers.values():
+  print(f"{river.title()}")
+

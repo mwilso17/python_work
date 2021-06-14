6d0c1003a2eb43d2208207c34e34ea0f96a71d7a
Mike Wilson <mwilso17@my.westga.edu>
2021-06-14 06:09:34 -0400

    made dictionary and key value pairs

--- dictionaries/person.py
+++ dictionaries/person.py
@@ -2,3 +2,9 @@
 # This program demonstrates how to build a dictionary and how to call upon
 # its contents.
 
+person = {
+  'first_name': 'tony',
+  'last_name': 'stark',
+  'age': '42',
+  'city': 'new york city' 
+}

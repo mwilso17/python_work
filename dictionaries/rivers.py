66e69c6bfc08cc840d6468676add4f228eb8553b
Mike Wilson <mwilso17@my.westga.edu>
2021-06-14 07:15:42 -0400

    added third 'for' loop to print countries used

--- dictionaries/rivers.py
+++ dictionaries/rivers.py
@@ -11,7 +11,10 @@ rivers = {
 for country, river in rivers.items():
   print(f"\nThe {river.title()} river runs through {country.title()}.")
 
-print("The following rivers can be found in the dictionary:")
+print("\nThe following rivers can be found in the dictionary:")
 for river in rivers.values():
   print(f"{river.title()}")
 
+print("\nThe following countries can be found in the dictionary:")
+for country in rivers.keys():
+  print(f"{country.title()}")

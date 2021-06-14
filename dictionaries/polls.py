1ce7f9a139fb2ee0947c169f88225b390801ecb3
Mike Wilson <mwilso17@my.westga.edu>
2021-06-14 07:22:24 -0400

    new file, initial comment, initial dictionary

---
+++ dictionaries/polls.py
@@ -0,0 +1,10 @@
+# Mike Wilson 14 June 2021
+# This program has a dictionary and uses for loops with if statements in order
+# to see if someone has taken a poll or not.
+
+favorite_pizzas = {
+  'mike': 'pepperoni',
+  'danny': 'mushroom',
+  'ash': 'cheese',
+  'john': 'supreme'
+}

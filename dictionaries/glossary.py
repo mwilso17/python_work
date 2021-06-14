54a36e7de297cac5f6d625817dae422620fec077
Mike Wilson <mwilso17@my.westga.edu>
2021-06-14 06:29:37 -0400

    made dictionary

--- dictionaries/glossary.py
+++ dictionaries/glossary.py
@@ -1,2 +1,10 @@
 # Mike Wilson 14 June 2021
 # This program mimics an actual dictionary
+
+glossary = {
+  'boolean': 'an expression that can be evaluated as either true or false.',
+  'string': 'a series of characters.',
+  'dictionary': 'a set of key value pairs.',
+  'function': 'a block of code that performs a task.',
+  'list': 'a collection of items in a certain order.',
+}

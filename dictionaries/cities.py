81a518ca8fbcb00671b8725d716bae34bdbd7370
Mike Wilson <mwilso17@my.westga.edu>
2021-06-15 06:45:05 -0400

    for loop and print

--- dictionaries/cities.py
+++ dictionaries/cities.py
@@ -4,14 +4,14 @@
 
 cities = {
   'atlanta': {
-    'country': 'USA',
+    'country': 'the USA',
     'population': 1_000_000,
-    'fact': 'home to Coca-Cola',
+    'fact': 'is home to Coca-Cola.',
   },
   'carrollton': {
-    'country': 'USA',
+    'country': 'the USA',
     'population': 15_000,
-    'fact': 'home to Southwire',
+    'fact': 'is home to Southwire.',
   },
   'oldenburg': {
     'country': 'Germany',
@@ -19,3 +19,12 @@ cities = {
     'fact': 'is over 600 years old!'
   },
 }
+
+for city, city_info in cities.items():
+  country = city_info['country']
+  population = city_info['population']
+  fact = city_info['fact']
+
+  print(f"\n{city.title()} is in {country}.")
+  print(f"  It has a population of about {population}.")
+  print(f"  {city.title()} {fact}")

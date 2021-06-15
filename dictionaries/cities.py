2fad587b4315f7e952b27a0cf79027fcb7457ea4
Mike Wilson <mwilso17@my.westga.edu>
2021-06-15 06:40:28 -0400

    updated comment, added nested dictionary

--- dictionaries/cities.py
+++ dictionaries/cities.py
@@ -1,3 +1,21 @@
 # Mike Wilson 15 June 2021
-# This program has cities in a dictionary and prints data stored in the dictionary
+# This program has cities in a dictionary and prints data stored in each
+# city's dictionary. (Nested dictionaries)
 
+cities = {
+  'atlanta': {
+    'country': 'USA',
+    'population': 1_000_000,
+    'fact': 'home to Coca-Cola',
+  },
+  'carrollton': {
+    'country': 'USA',
+    'population': 15_000,
+    'fact': 'home to Southwire',
+  },
+  'oldenburg': {
+    'country': 'Germany',
+    'population': 100_000,
+    'fact': 'is over 600 years old!'
+  },
+}

af98711726c48ec67f917bc0461913be42ce1fbf
Mike Wilson <mwilso17@my.westga.edu>
2021-06-15 06:58:46 -0400

    added code to take user input

--- user input and while loops/rental_car.py
+++ user input and while loops/rental_car.py
@@ -2,3 +2,6 @@
 # This program takes user input to see what type of car they want to rent
 # and repeats a message back to them.
 
+car_type = input("What type of rental car would you like? ")
+
+print(f"Let me see if we have any {car_type}s in stock.")

b1040478b43de71f9dee5a70c3e5deea97f48977
Mike Wilson <mwilso17@my.westga.edu>
2021-06-15 07:15:55 -0400

    if else statement added

--- user input and while loops/multiples_of_ten.py
+++ user input and while loops/multiples_of_ten.py
@@ -5,3 +5,7 @@
 number = input("Please enter a number: ")
 number = int(number)
 
+if number % 10 == 0:
+  print(f"\nThe number {number} is divisible by 10.")
+else:
+  print(f"\nThe number is not divisible by 10.")

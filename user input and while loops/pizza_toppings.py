b7ccd12c85f0b91f87d2fc5c75c970c31049ef3d
Mike Wilson <mwilso17@my.westga.edu>
2021-06-15 07:38:39 -0400

    updated code to fix bug

--- user input and while loops/pizza_toppings.py
+++ user input and while loops/pizza_toppings.py
@@ -1,14 +1,14 @@
 # Mike Wilson 15 June 2021
 # This program takes user input and simulates adding their input to a pizza.
 
-prompt = "\nWhat type of toppings would you like on your pizza? "
-prompt += "\nEnter 'quit' to stop."
+prompt = "\nWhat type of toppings would you like on your pizza?"
+prompt += "\nEnter 'quit' to stop. "
 
-toppings = ""
-while toppings != 'quit':
+
+while True:
   toppings = input(prompt)
-  print(f"Adding {toppings} to your pizza!")
-
-  if toppings == 'quit':
-    print(f"Making your pizza now!")
-    break
+  if toppings != 'quit':
+    print(f"Adding {toppings} to your pizza now!")
+  else:
+      print(f"Making your pizza now!")
+      break

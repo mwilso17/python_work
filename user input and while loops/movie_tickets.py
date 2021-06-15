d1da3765ae13caf9fffba37519d0118e298798c5
Mike Wilson <mwilso17@my.westga.edu>
2021-06-15 07:55:13 -0400

    updated code. added while loop.

--- user input and while loops/movie_tickets.py
+++ user input and while loops/movie_tickets.py
@@ -2,16 +2,23 @@
 # This program takes user input for age then returns the price of their movie
 # ticket to them.
 
-age = input("\nWhat is your age? ")
-age = int(age)
+prompt = "\nWhat is your age? "
+prompt += "\nEnter 'quit' when you are finished."
 
-if age < 3:
-  print("Your ticket is free.")
-elif age < 12:
-  print("Your ticket is $10.")
-elif age < 65:
-  print("Your ticket is $15.")
-elif age < 400:
-  print("You get a senior discount. Your ticket is $12.")
-elif age >= 400:
-  print("Sorry, we don't let vampires into this theater.")
+
+while True:
+  age = input(prompt)
+  if age == 'quit':
+    break
+  age = int(age)
+
+  if age < 3:
+    print("Your ticket is free.")
+  elif age < 12:
+    print("Your ticket is $10.")
+  elif age < 65:
+    print("Your ticket is $15.")
+  elif age < 400:
+    print("You get a senior discount. Your ticket is $12.")
+  elif age >= 400:
+    print("Sorry, we don't let vampires into this theater.")

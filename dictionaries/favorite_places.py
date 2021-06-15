615e93c39ace16ca8260301fa243c26a923b5fcc
Mike Wilson <mwilso17@my.westga.edu>
2021-06-15 06:22:59 -0400

    made dictionary

--- dictionaries/favorite_places.py
+++ dictionaries/favorite_places.py
@@ -1,3 +1,9 @@
 # Mike Wilson 15 June 2021
 # This program uses a dictionary to print people's favorite places.
 
+favorite_places = {
+  'mike': ['munich', 'kyoto', 'atlanta'],
+  'ash': ['berlin', 'paris', 'london'],
+  'rob': ['phoenix', 'sydney', 'olso'],
+}
+

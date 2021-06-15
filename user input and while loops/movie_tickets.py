4f05d0e55a28d7ef9f95a4f792f2dc0041b125c7
Mike Wilson <mwilso17@my.westga.edu>
2021-06-15 07:50:11 -0400

    if-elif chain

--- user input and while loops/movie_tickets.py
+++ user input and while loops/movie_tickets.py
@@ -4,3 +4,14 @@
 
 age = input("\nWhat is your age? ")
 age = int(age)
+
+if age < 3:
+  print("Your ticket is free.")
+elif age < 12:
+  print("Your ticket is $10.")
+elif age < 65:
+  print("Your ticket is $15.")
+elif age >= 65:
+  print("You get a senior discount. Your ticket is $12.")
+elif age >= 400:
+  print("Sorry, we don't let vampires into this theater.")

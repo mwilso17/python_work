5fb290e5c42649e3a5b02d69e9f72f9832b0ae55
Mike Wilson <mwilso17@my.westga.edu>
2021-06-15 06:25:04 -0400

    for loop nested in a for loop

--- dictionaries/favorite_places.py
+++ dictionaries/favorite_places.py
@@ -7,3 +7,7 @@ favorite_places = {
   'rob': ['phoenix', 'sydney', 'olso'],
 }
 
+for name, places in favorite_places.items():
+  print(f"\n{name.title()}'s favorite places are: ")
+  for place in places:
+    print(f"{place.title()}")

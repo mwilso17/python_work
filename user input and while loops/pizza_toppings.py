6690440c938115a1ffad0144d29dd5aeb0829688
Mike Wilson <mwilso17@my.westga.edu>
2021-06-15 07:31:58 -0400

    initial prompt added

--- user input and while loops/pizza_toppings.py
+++ user input and while loops/pizza_toppings.py
@@ -1,2 +1,6 @@
 # Mike Wilson 15 June 2021
 # This program takes user input and simulates adding their input to a pizza.
+
+prompt = "\nWhat type of toppings would you like on your pizza? "
+prompt += "\nEnter 'quit' to stop."
+

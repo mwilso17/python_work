4afda335780f9cdc03656940a1daf414e4e1bb0c
Mike Wilson <mwilso17@my.westga.edu>
2021-06-17 06:59:33 -0400

    created function, called function

--- functions/message.py
+++ functions/message.py
@@ -1,2 +1,9 @@
 # Mike Wilson 17 June 2021
 # This program demonstrates how to build a function.
+
+def display_message():
+  """This function displays a message"""
+  print("I am learning how to make functions in Python!")
+
+
+display_message()

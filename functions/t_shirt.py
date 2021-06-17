33d64966293200e0be19d2ff6c1e0cee20c734f2
Mike Wilson <mwilso17@my.westga.edu>
2021-06-17 07:35:20 -0400

    new file, comment, function, called function

---
+++ functions/t_shirt.py
@@ -0,0 +1,9 @@
+# Mike Wilson 17 June 2021
+# This program simulates a t-shirt maker using a function to accept
+# arguments to make a certain sized shirt with a message on it.
+
+def make_shirt(size, message):
+  """make the shirt with a message on it"""
+  print(f"Making your {size} shirt that says {message}")
+
+make_shirt('small', 'I love ATL')

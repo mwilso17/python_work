53a1fc309f4128f2abd1d1148127216984a3cf48
Mike Wilson <mwilso17@my.westga.edu>
2021-06-17 06:33:40 -0400

    added while loop for prompts

--- user input and while loops/dream_vacation.py
+++ user input and while loops/dream_vacation.py
@@ -3,9 +3,19 @@
 
 # 3 prompts to be used
 name_prompt = "\nWhat is your name? "
-vacation_prompt = "If you could go anywhere in the world, where would you go? "
-next_prompt = "\nWould someone else like to take the poll? "
+vacation_prompt = "If you could vacation anywhere in the world, where would you go? "
+next_prompt = "\nWould someone else like to take the poll? (yes/no)"
 
 # empty dictionary to store responses in
 responses = {}
+
+while True:
+  name = input(name_prompt)
+  vacation = input(vacation_prompt)
+
+  responses[name] = vacation
+
+  repeat = input(next_prompt)
+  if repeat != 'yes':
+    break
 

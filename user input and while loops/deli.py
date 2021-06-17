69b76a80c5dc2f31e53421f12844c13f339ddbe5
Mike Wilson <mwilso17@my.westga.edu>
2021-06-17 06:16:52 -0400

    added 2 lists

--- user input and while loops/deli.py
+++ user input and while loops/deli.py
@@ -1,3 +1,9 @@
 # Mike Wilson 17 June 2021
 # This program simulates a sandwich shop that takes sandwich orders
 # and moves them to a list of finished sandwiches.
+
+# list for sandwich orders
+sandwich_orders = ['club', 'ham and swiss', 'pastrami', 'turkey', 'veggie']
+
+# empty list for finished sandwiches
+finished_sandwiches = []

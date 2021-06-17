dfa99c41222c23b69f6f68e14cb08939281d1c43
Mike Wilson <mwilso17@my.westga.edu>
2021-06-17 06:28:39 -0400

    new file, initial comment, 3 prompts

---
+++ user input and while loops/dream_vacation.py
@@ -0,0 +1,7 @@
+# Mike Wilson 17 June 2021
+# The following program polls users about their dream vacations.
+
+# 3 prompts to be used
+name_prompt = "\nWhat is your name? "
+vacation_prompt = "If you could go anywhere in the world, where would you go? "
+next_prompt = "\nWould someone else like to take the poll? "

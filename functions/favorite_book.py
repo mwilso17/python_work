1ab3459242d905a1a92d60abafeb220b4f354665
Mike Wilson <mwilso17@my.westga.edu>
2021-06-17 07:16:51 -0400

    added fuction, called function

--- functions/favorite_book.py
+++ functions/favorite_book.py
@@ -1,3 +1,9 @@
 # Mike Wilson 17 June 2021
 # This program has a function that accepts a parameter.
 
+def favorite_book(title):
+  """function to display someone's favorite book"""
+  print(f"My favorite book is {title.title()}.")
+
+
+favorite_book('Lord of the Rings')

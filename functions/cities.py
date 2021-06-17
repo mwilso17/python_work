09b5b34a6dadb0a0c0eccca75f25f2ea26643ee0
Mike Wilson <mwilso17@my.westga.edu>
2021-06-17 07:52:07 -0400

    added function

--- functions/cities.py
+++ functions/cities.py
@@ -1,3 +1,6 @@
 # Mike Wilson 17 June 2021
 # This program describes various cities using a function.
 
+def describe_city(name, country='germany'):
+  """prints name of city and country"""
+  print(f"{name.title()} is located in {country.title()}")

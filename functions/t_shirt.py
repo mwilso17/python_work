254f93db79b0ef2cc2a19f2663df2bb4033af5ee
Mike Wilson <mwilso17@my.westga.edu>
2021-06-17 07:43:27 -0400

    added defaults to function, multiple function call

--- functions/t_shirt.py
+++ functions/t_shirt.py
@@ -2,7 +2,7 @@
 # This program simulates a t-shirt maker using a function to accept
 # arguments to make a certain sized shirt with a message on it.
 
-def make_shirt(size, message):
+def make_shirt(size='large', message='I love Python!'):
   """make the shirt with a message on it"""
   print(f"Making your {size} shirt that says '{message}'.")
 
@@ -11,3 +11,9 @@ make_shirt('small', 'I love ATL')
 
 # call function with keyword arguments
 make_shirt(size='medium', message='Python is fun!')
+
+# calling function multiple times
+make_shirt()
+make_shirt()
+make_shirt(size='x-large')
+make_shirt(size='x-small', message='Functions are cool!')

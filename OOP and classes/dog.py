3bf75e846caecba10ba358010847abfa5083d5cd
Mike Wilson <mwilso17@my.westga.edu>
2021-06-20 09:11:47 -0400

    added class with 3 functions

--- OOP and classes/dog.py
+++ OOP and classes/dog.py
@@ -1,2 +1,18 @@
 # Mike Wilson 20 June 2021
-# This program attempts to make a model of a dog
+# This program attempts to make a model of a dog.
+
+class Dog:
+  """a simple attempt to model a dog"""
+
+  def __init__(self, name, age):
+    """Initialize name and age attributes."""
+    self.name = name
+    self.age = age
+
+  def sit(self):
+    """Simulates dog sitting down"""
+    print(f"{self.name} is now sitting")
+
+  def roll_over(self):
+    """Simulate rolling over"""
+    print(f"{self.name} rolled over!")

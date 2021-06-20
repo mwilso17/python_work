c396369e3182ceb69db80b06596a41347100a766
Mike Wilson <mwilso17@my.westga.edu>
2021-06-20 08:07:24 -0400

    called function

--- functions/user_profile.py
+++ functions/user_profile.py
@@ -7,3 +7,6 @@ def build_profile(first, last, **user_info):
   user_info['last_name'] = last
   return user_info
 
+user_profile = build_profile('mike', 'wilson', location='atlanta',
+languages='python')
+print(user_profile)

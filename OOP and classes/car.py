97db1c05d883c929869cb8565a8aa20e4a0fa62b
Mike Wilson <mwilso17@my.westga.edu>
2021-06-20 10:58:09 -0400

    added new attribute & method. called method

--- OOP and classes/car.py
+++ OOP and classes/car.py
@@ -9,11 +9,17 @@ class Car:
     self.make = make
     self.model = model
     self. year = year
+    self.odometer_reading = 0
 
   def get_descriptive_name(self):
     """Returns a neatly formatted descriptive name."""
     descriptive_name = f"{self.year} {self.make} {self.model}"
     return descriptive_name.title()
 
+  def read_odometer(self):
+    """Print a statement showing the car's mileage"""
+    print(f"This car has {self.odometer_reading} miles on it.")
+
 my_old_car = Car('honda', 'accord', 1995)
 print(my_old_car.get_descriptive_name())
+my_old_car.read_odometer()

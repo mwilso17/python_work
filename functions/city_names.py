f76331de5118e712cb611692e357f2ebdfb32f2a
Mike Wilson <mwilso17@my.westga.edu>
2021-06-20 06:59:52 -0400

    added and called function

--- functions/city_names.py
+++ functions/city_names.py
@@ -2,3 +2,15 @@
 # This program has a function that takes a city and country name and returns
 # a string.
 
+def city_name(city, country):
+  """Return a string for a city, country pair"""
+  return f"{city.title()}, {country.title()}"
+
+city = city_name('berlin', 'germany')
+print(city)
+
+city = city_name('olso', 'norway')
+print(city)
+
+city = city_name('paris', 'france')
+print(city)

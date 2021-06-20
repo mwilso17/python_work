6c666c7a86551eb6e9682281a2a8543a5925297c
Mike Wilson <mwilso17@my.westga.edu>
2021-06-20 10:37:56 -0400

    created class for User

--- OOP and classes/users.py
+++ OOP and classes/users.py
@@ -1,3 +1,21 @@
 # Mike Wilson 20 June 2021
 # this program simulates the creation of a user profile.
 
+class User:
+  """create a user profile"""
+
+  def __init__(self, first, last, user_name):
+    """initialize self and attributes"""
+    self.first = first
+    self.last = last
+    self.user_name = user_name
+
+  def describe_user(self):
+    """describes the user"""
+    print(f"\nFirst Name: {self.first.title()}")
+    print(f"Last Name: {self.last.title()}")
+    print(f"Username: {self.user_name}")
+
+  def greet_user(self):
+    """greets the user"""
+    print(f"Welcome, {self.first.title()}!")

02cf521f1e6e5655da282f90eedd376f50ef9223
Mike Wilson <mwilso17@my.westga.edu>
2021-06-20 10:50:16 -0400

    Car class with 2 methods

--- OOP and classes/car.py
+++ OOP and classes/car.py
@@ -1,3 +1,17 @@
 # Mike Wilson 20 June 2021
 # At attempt at representing a car.
 
+class Car:
+  """An attempt to represent a car"""
+
+  def __init__(self, make, model, year):
+    """Initialize attributes"""
+    self.make = make
+    self.model = model
+    self. year = year
+
+  def get_descriptive_name(self):
+    """Returns a neatly formatted descriptive name."""
+    descriptive_name = f"{self.year} {self.make} {self.model}"
+    return descriptive_name.title()
+

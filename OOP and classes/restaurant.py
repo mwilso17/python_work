c6cba6c759d99fc50b73987c535c0f5b012da3a1
Mike Wilson <mwilso17@my.westga.edu>
2021-06-20 09:35:15 -0400

    made new class

--- OOP and classes/restaurant.py
+++ OOP and classes/restaurant.py
@@ -1,2 +1,18 @@
 # Mike Wilson 20 June 2021
 # This program creates a class that models a restuarant. 
+
+class Restaurant:
+  """a model of a restaurant"""
+
+  def __init___(self, name, cuisine):
+    """initialize name and cuisine attributes"""
+    self.name = name
+    self.cuisine = cuisine
+
+  def describe_restaurant(self):
+    """describes the restaurant"""
+    print(f"{self.name} is a {self.cuisine} restaurant.")
+
+  def open_restaurant(self):
+    """opens the restaurant for business"""
+    print(f"{self.name} is now open for business!")

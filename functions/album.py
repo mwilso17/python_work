9dc3491732aad3e4781535d028c0520282a17f04
Mike Wilson <mwilso17@my.westga.edu>
2021-06-20 07:14:25 -0400

    fixed bug in function

--- functions/album.py
+++ functions/album.py
@@ -1,7 +1,7 @@
 # Mike Wilson 20 June 2021
 # This program builds a dictionary discribing an album.
 
-def make_album(artist, title, tracks='0):
+def make_album(artist, title, tracks=0):
   """Build a dictionary containing info about an album"""
   album_dictionary = {
     'artist': artist.title(),

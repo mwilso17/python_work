65bb30073ba1cdaf5d238036188a8ac21fbc8202
Mike Wilson <mwilso17@my.westga.edu>
2021-06-20 07:08:43 -0400

    made function

--- functions/album.py
+++ functions/album.py
@@ -1,3 +1,13 @@
 # Mike Wilson 20 June 2021
 # This program builds a dictionary discribing an album.
 
+def make_album(artist, title, tracks='0):
+  """Build a dictionary containing info about an album"""
+  album_dictionary = {
+    'artist': artist.title(),
+    'title': title.title(),
+  }
+  if tracks:
+    album_dictionary['tracks'] = tracks
+  return album_dictionary
+

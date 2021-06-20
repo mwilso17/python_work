8dce8816e714dfe29f9356d60b5a84522e5b41e3
Mike Wilson <mwilso17@my.westga.edu>
2021-06-20 08:35:54 -0400

    added function

--- functions/cars.py
+++ functions/cars.py
@@ -1,2 +1,15 @@
 # Mike Wilson 20 June 2021
 # This program stores info about a car in  a dictionary. 
+
+def make_car(make, model, *other):
+  """makes a dictionary for a car"""
+  car_dictionary = {
+    'make' = make.title(),
+    'model' = model.title(),
+  }
+
+  for other, value in other.items():
+    car_dictionary[other] = value
+
+  return car_dictionary
+

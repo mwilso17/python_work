005e5c70906ba7733c5ad8215f3f387461192367
Mike Wilson <mwilso17@my.westga.edu>
2021-06-20 08:45:09 -0400

    added final function

--- functions/printing_functions.py
+++ functions/printing_functions.py
@@ -11,3 +11,9 @@ def print_models(unprinted_designs, completed_models):
     # Simulate a printed item from the 3d printer
     print(f"Printing model: {current_design}")
     completed_models.append(current_design)
+
+def show_completed_models(completed_models):
+  """Show all models that have been completed"""
+  print("\nThe following models have been printed:")
+  for completed_model in completed_models:
+    print(completed_model)

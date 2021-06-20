3e7e96a0e7ee36d92172cbc56a1870445a5af773
Mike Wilson <mwilso17@my.westga.edu>
2021-06-20 08:05:55 -0400

    added function

--- functions/user_profile.py
+++ functions/user_profile.py
@@ -1,3 +1,9 @@
 # Mike Wilson 20 June 2021
 # This program builds a dictionary containing info we know about a person.
 
+def build_profile(first, last, **user_info):
+  """Build a disctionary containing a person's info"""
+  user_info['first_name'] = first
+  user_info['last_name'] = last
+  return user_info
+

a5e6649e8bf62c729514de4fcf4c40aa379336da
Mike Wilson <mwilso17@my.westga.edu>
2021-06-20 07:35:47 -0400

    added and called function. added list

--- functions/messages.py
+++ functions/messages.py
@@ -1,3 +1,10 @@
 # Mike Wilson 20 June 2021
 # This program has a list of short messages and displays them.
 
+def show_messages(messages):
+  """print messages in  list"""
+  for message in messages:
+    print(message)
+
+messages = ['hey!', 'What\'s up?', ':)']
+show_messages(messages)

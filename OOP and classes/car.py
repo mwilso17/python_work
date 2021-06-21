e2594574188f05b5c3ae6bb8855f925d4292bf05
Mike Wilson <mwilso17@my.westga.edu>
2021-06-21 06:43:53 -0400

    added class for battery

--- OOP and classes/car.py
+++ OOP and classes/car.py
@@ -39,13 +39,24 @@ class Car:
     print(f"This {self.make.title()} now has a full tank of gas!")
 
 
+class Battery:
+  """An attempt to model a battery for an electric car."""
+  def __init__(self, battery_size=75):
+    """Initializes battery attributes"""
+    self.battery_size = battery_size
+
+  def describe_battery(self):
+    """Print a statement describing the battery size"""
+    print(f"This car has a {self.battery_size}-kWh battery.")
+
+
 class ElectricCar(Car):
   """Represents aspects of a car, specific to electric cars"""
 
   def __init__(self, make, model, year):
     """Initialize attributes of the parent class"""
     super().__init__(make, model, year)
-    self.battery_size = 75
+    self.describe_battery = Battery()
 
   def describe_battery(self):
     """Prints a statement describing battery size"""

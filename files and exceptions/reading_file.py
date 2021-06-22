1ae3fe208f15332c36c74f26e0d6ae5ba8760002
Mike Wilson <mwilso17@my.westga.edu>
2021-06-22 07:36:32 -0400

    moved learning python to new folder

--- files and exceptions/reading_file.py
+++ files and exceptions/reading_file.py
@@ -1,2 +1,8 @@
 # Mike Wilson 22 June 2021
 # This program reads the file learning_python.txt contained in this folder.
+
+
+print("--- Reading the entire file:")
+with open('learning_python.txt') as f:
+  contents = f.read()
+print(contents)

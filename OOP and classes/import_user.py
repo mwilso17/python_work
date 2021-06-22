9bffa59571107b63b36d7eacd5a8a874f9f22043
Mike Wilson <mwilso17@my.westga.edu>
2021-06-22 06:11:36 -0400

    made instance of class, called methods from import

--- OOP and classes/import_user.py
+++ OOP and classes/import_user.py
@@ -1,3 +1,15 @@
 # Mike Wilson 22 June 2021
-# This program imports users.py and calls upon its classes and methods.
+# This program imports users.py and calls upon its class 'Admin'.
 
+from users import Admin
+
+mike = Admin('mike', 'wilson', 'mmwww2413')
+mike.describe_user()
+
+mike_privileges = [
+  'can reset passwords',
+  'can delete accounts',
+]
+mike.privileges.privileges = mike_privileges
+
+mike.privileges.show_privileges()

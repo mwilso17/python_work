a7ec585802a36817c23e279d958c7672a3bd9827
Mike Wilson <mwilso17@my.westga.edu>
2021-06-22 07:42:53 -0400

    stored lines in a list

--- files and exceptions/reading_file.py
+++ files and exceptions/reading_file.py
@@ -12,3 +12,11 @@ print("\n--- Looping over the lines in the file.")
 with open(filename) as f:
   for line in f:
     print(line.rstrip())
+
+print("\n--- Storing the lines in a list:")
+with open(filename) as f:
+  lines = f.readlines()
+
+
+for line in lines:
+  print(line.rstrip())

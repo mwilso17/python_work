e63357bb4bc3e198ca99ac94356204f344adb153
Mike Wilson <mwilso17@my.westga.edu>
2021-06-22 06:47:13 -0400

    added while loop with if statement

--- OOP and classes/lottery.py
+++ OOP and classes/lottery.py
@@ -7,3 +7,13 @@ possibilities = [1, 2 ,3 ,4, 5, 6, 7, 8, 9, 10, 'a', 'b', 'c', 'd', 'e']
 
 winning_ticket = []
 print("And the winning ticket is...... ")
+
+# While loop to keep numbers and letters from repeating.
+while len(winning_ticket) < 4:
+  pulled_item = choice(possibilities)
+
+  #Only adds pulled item to winning_ticket if not yet pulled
+  if pulled_item not in winning_ticket:
+    winning_ticket.append(pulled_item)
+
+

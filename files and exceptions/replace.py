e3d89c86fe7d79266f3c04a7bc2b5de30e3db8ed
Mike Wilson <mwilso17@my.westga.edu>
2021-06-22 07:48:00 -0400

    displayed contents of file 1 time

--- files and exceptions/replace.py
+++ files and exceptions/replace.py
@@ -3,3 +3,8 @@
 # 'txt_files' for the exact document used.
 
 filename = 'txt_files\learning_python.txt'
+
+print("--- Displaying unchanged file one time:")
+with open(filename) as file:
+  contents = file.read()
+print(contents)

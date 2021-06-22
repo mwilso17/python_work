87c1f5ad2fcd2ae45434ed903c87dcd93aa99202
Mike Wilson <mwilso17@my.westga.edu>
2021-06-22 07:39:51 -0400

    called on file and printed read its contents

--- files and exceptions/reading_file.py
+++ files and exceptions/reading_file.py
@@ -1,8 +1,9 @@
 # Mike Wilson 22 June 2021
 # This program reads the file learning_python.txt contained in this folder.
 
+filename = 'txt_files\learning_python.txt'
 
-print("--- Reading the entire file:")
-with open('learning_python.txt') as f:
-  contents = f.read()
+print("--- Reading in the entire file:")
+with open(filename) as f:
+    contents = f.read()
 print(contents)

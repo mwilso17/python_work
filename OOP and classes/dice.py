691ba40849634ee5370743248313a9374388690c
Mike Wilson <mwilso17@my.westga.edu>
2021-06-22 06:25:55 -0400

    class Die to simulate a die.

--- OOP and classes/dice.py
+++ OOP and classes/dice.py
@@ -4,3 +4,13 @@
 
 from random import randint
 
+class Die:
+  """Represents a die that can be rolled."""
+
+  def __init__(self, sides=6):
+    """Initialize the die. 6 by default."""
+    self.sides = sides
+
+  def roll_die(self):
+    """Return a number between 1 and the number of sides."""
+    return randint(1, self.sides)

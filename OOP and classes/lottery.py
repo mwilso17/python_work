2908edd7c555a306ef90bf2f4a8a0c38686fa0c9
Mike Wilson <mwilso17@my.westga.edu>
2021-06-22 06:43:20 -0400

    set lists for possibilities and winning_ticket

--- OOP and classes/lottery.py
+++ OOP and classes/lottery.py
@@ -1,3 +1,9 @@
 # Mike Wilson 22 June 2021
 # This program simulates a lottery and displays the 'winning' combination.
 
+from random import choice
+
+possibilities = [1, 2 ,3 ,4, 5, 6, 7, 8, 9, 10, 'a', 'b', 'c', 'd', 'e']
+
+winning_ticket = []
+print("And the winning ticket is...... ")

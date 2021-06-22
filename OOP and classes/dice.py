113b1985510f994824b2a0cb2e3b77be3956077e
Mike Wilson <mwilso17@my.westga.edu>
2021-06-22 06:32:28 -0400

    made a 6-sided dice. rolled 10 times

--- OOP and classes/dice.py
+++ OOP and classes/dice.py
@@ -14,3 +14,15 @@ class Die:
   def roll_die(self):
     """Return a number between 1 and the number of sides."""
     return randint(1, self.sides)
+
+
+# Make a 6 sided die and roll it 10 times.
+d6 = Die()
+
+results = []
+for roll_num in range(10):
+  result = d6.roll_die()
+  results.append(result)
+
+print("The 10 rolls of a 6 sided dice came up: ")
+print(results)

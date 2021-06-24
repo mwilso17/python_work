9f693402745b79fc27ac3be76be839da116f0be6
Mike Wilson <mwilso17@my.westga.edu>
2021-06-24 07:32:46 -0400

    code allowing users to store poll answers in txt

--- files and exceptions/programming_poll.py
+++ files and exceptions/programming_poll.py
@@ -2,3 +2,17 @@
 # This program uses a while loop to ask people why the enjoy programming.
 # Responses are stored in 'txt_files\programming_poll.txt'.
 
+filename = 'txt_files\programming_poll.txt'
+
+responses = []
+while True:
+  response = input("\nWhy do you like programming? ")
+  responses.append(response)
+
+  continue_poll = input("Would you like to let someone else respond? (y/n) ")
+  if continue_poll != 'y':
+    break
+
+with open(filename, 'a') as f:
+  for response in responses:
+    f.write(f"{response}\n")

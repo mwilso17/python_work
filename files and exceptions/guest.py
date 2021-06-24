460bb7ea58c78735fd2188af5098d98162bc1e62
Mike Wilson <mwilso17@my.westga.edu>
2021-06-24 07:11:50 -0400

    gave users ability to write to file.

--- files and exceptions/guest.py
+++ files and exceptions/guest.py
@@ -2,3 +2,9 @@
 # This program prompts a user for their name then writes it to
 # txt_files\guest.txt.
 
+name = input("What's your name? ")
+
+filename = 'txt_files\guest.txt'
+
+with open(filename, 'w') as f:
+  f.write(name)

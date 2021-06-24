9a595d1900fb988b10d0bcf59829f3fbe3faf47a
Mike Wilson <mwilso17@my.westga.edu>
2021-06-24 07:13:51 -0400

    changes guest.txt to guests.txt

--- files and exceptions/guest.py
+++ files and exceptions/guest.py
@@ -1,10 +1,10 @@
 # Mike Wilson 24 June 2021
 # This program prompts a user for their name then writes it to
-# txt_files\guest.txt.
+# txt_files\guests.txt.
 
 name = input("What's your name? ")
 
-filename = 'txt_files\guest.txt'
+filename = 'txt_files\guests.txt'
 
 with open(filename, 'w') as f:
   f.write(name)

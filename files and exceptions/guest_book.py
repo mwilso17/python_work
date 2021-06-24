29f75321acaf8eafef838958badd005e232cfe8f
Mike Wilson <mwilso17@my.westga.edu>
2021-06-24 07:19:35 -0400

    added while loop

--- files and exceptions/guest_book.py
+++ files and exceptions/guest_book.py
@@ -1,3 +1,15 @@
 # Mike Wilson 24 June 2021
 # This program uses a while loop to prompt users for their name and 
 # prints a greeting. Entries are stored in 'txt_files\guest_book.txt'.
+
+filename = 'txt_files\guest_book.txt'
+
+print("Enter 'q' to exit the program")
+while True:
+  name = input("\nWhat is your name? ")
+  if name == 'q':
+    break
+  else:
+    with open(filename, 'a') as f:
+      f.write(f"{name}\n")
+    print(f"Hi {name.title()}. You have been added to the guest book!")

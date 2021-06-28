d7ee1e6ac3110d5b7b2ca70bfe6c2af02c3dafe6
Mike Wilson <mwilso17@my.westga.edu>
2021-06-28 07:33:22 -0400

    added filenames

--- files and exceptions/cats_and_dogs.py
+++ files and exceptions/cats_and_dogs.py
@@ -1,3 +1,5 @@
 # Mike Wilson 28 June 2021
 # This program analyzes two different txt files from the txt_files folder.
 # This code catches the FileNotFound error.
+
+filenames = ['txt_files\cats.txt', 'txt_files\dogs.txt']

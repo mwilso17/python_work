4387ff2f6f12be38e118b1622fe0978ac1a7b1f0
Mike Wilson <mwilso17@my.westga.edu>
2021-06-28 07:54:47 -0400

    added function to count common words

--- files and exceptions/common_words.py
+++ files and exceptions/common_words.py
@@ -2,3 +2,17 @@
 # This program analyzes txt files for common words. The file used in this
 # example comes from gutenberg.org, all rights reserved. This program is to be
 # used strictly for educational and informational purposes.
+
+def count_common_words(filename, word):
+  """Counts how many time a word appears in a txt file."""
+  try:
+    with open(filename) as f:
+      contents = f.read()
+
+  except FileNotFoundError:
+    pass
+
+  else:
+    word_count = contents.lower().count(word)
+
+    message = f"'{word}' appers in {filename} around {word_count} times."

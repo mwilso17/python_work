e7b4dc5c49f4d168c0d3314cdc2ee2a1d0823bde
Mike Wilson <mwilso17@my.westga.edu>
2021-06-28 07:51:46 -0400

    new file. initial comment

---
+++ files and exceptions/common_words.py
@@ -0,0 +1,4 @@
+# Mike Wilson 28 June 2021
+# This program analyzes txt files for common words. The file used in this
+# example comes from gutenberg.org, all rights reserved. This program is to be
+# used strictly for educational and informational purposes.

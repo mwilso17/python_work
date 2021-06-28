62895db13a5efad2a3be718bef80a6adf6897428
Mike Wilson <mwilso17@my.westga.edu>
2021-06-28 07:23:29 -0400

    try, except, else

--- files and exceptions/addition.py
+++ files and exceptions/addition.py
@@ -1,3 +1,17 @@
 # Mike Wilson 28 June 2021
 # This program takes user input for 2 numbers and adds them together.
 # It contains exceptions in case the user does not enter numbers.
+
+try:
+  x = input("Enter a number: ")
+  x = int(x)
+
+  y = input("Enter another number: ")
+  y = int(y)
+
+except ValueError:
+  print("Please enter a number. ex: 3")
+
+else:
+  sum = x + y
+  print(f"The sum of {x} and {y} is {sum}.")

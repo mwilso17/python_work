3b93e0c179013291fb637e8110d1170d1e81f914
Mike Wilson <mwilso17@my.westga.edu>
2021-06-28 07:35:01 -0400

    added code to call files

--- files and exceptions/cats_and_dogs.py
+++ files and exceptions/cats_and_dogs.py
@@ -3,3 +3,12 @@
 # This code catches the FileNotFound error.
 
 filenames = ['txt_files\cats.txt', 'txt_files\dogs.txt']
+
+for filename in filenames:
+  print(f"\nReading file: {filename}")
+  try:
+    with open(filename) as f:
+      contents = f.read()
+      print(contents)
+    except FileNotFoundError:
+      print("Sorry, but we cannot find that file.")

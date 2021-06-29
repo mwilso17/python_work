085594c289474fe52a2035191fd407cdae0117c7
Mike Wilson <mwilso17@my.westga.edu>
2021-06-29 06:26:40 -0400

    wrote code to read json folder

--- files and exceptions/read_favorite_number.py
+++ files and exceptions/read_favorite_number.py
@@ -1,3 +1,10 @@
 # Mike Wilson 29 June 2021
 # This program reads the answer from favorite_number.py with the answer
 # that was stored in the 'json' folder.
+
+import json
+
+with open('json\\favorite_number.json') as f:
+  number = json.load(f)
+
+print(f"I know your favorite number. It is {number}.")

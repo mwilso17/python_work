6e1fa0a860caf49b5345527a1fce54289282957b
Mike Wilson <mwilso17@my.westga.edu>
2021-06-29 07:37:40 -0400

    created class to manage settings of the game

---
+++ alien_invasion/settings.py
@@ -0,0 +1,12 @@
+# Mike Wilson 29 June 2021
+# A file to hold the settings for Alien Invasion.
+
+class Settings:
+  """A class to store all settings for Alien Invasion"""
+
+  def __init__(self):
+    """Initialize the game's settings."""
+    # Screen Settings
+    self.screen_width = 1200
+    self. screen_height = 800
+    self.bg_color = (230, 230, 230)

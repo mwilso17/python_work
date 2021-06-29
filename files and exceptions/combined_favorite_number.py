1d3c6066bb88aff754f4a58222fbd27f1cef8166
Mike Wilson <mwilso17@my.westga.edu>
2021-06-29 06:32:39 -0400

    code to store and retreive answers

--- files and exceptions/combined_favorite_number.py
+++ files and exceptions/combined_favorite_number.py
@@ -1,3 +1,16 @@
 # Mike Wilson 29 June 2021
 # This program combines the code from read_favorite_number.py and
 # favorite_number.py. 
+
+import json
+
+try:
+  with open('json\\favorite_number2.json') as f:
+    number = json.load(f)
+except FileNotFoundError:
+  number = input("What is your favorite number? ")
+  with open('json\\favorite_number2.json', 'w') as f:
+    json.dump(number, f)
+  print("Ok. I will remember that.")
+else:
+  print(f"I remember your favorite number. It is {number}.")

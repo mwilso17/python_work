c033e62936f85fc6e6a28912d8d14ec088e831b4
Mike Wilson <mwilso17@my.westga.edu>
2021-06-29 06:16:13 -0400

    added code to get favorite number

--- files and exceptions/favorite_number.py
+++ files and exceptions/favorite_number.py
@@ -1,3 +1,11 @@
 # Mike Wilson 29 June 2021
 # This program prompts a user for a favorite number and stores the data
 # in a file that can later be retrieved.
+
+import json
+
+number = input("What is your favorite number? ")
+
+with open('favorite_number.json', 'w') as f:
+  json.dump(number, f)
+  print("Thanks. I will remember your favorite number.")

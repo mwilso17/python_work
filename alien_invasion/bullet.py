5abc076c164b009b3d505d77080c84c4b751dbb4
Mike Wilson <mwilso17@my.westga.edu>
2021-06-30 07:59:27 -0400

    added bullet class and updated settings for bullet

---
+++ alien_invasion/bullet.py
@@ -0,0 +1,23 @@
+# Mike Wilson 30 June 2021
+# This program manages the bullets for Alien_Invasion
+
+import pygame
+from pygame.sprite import Sprite
+
+class Bullet(Sprite):
+  """A class to manage bullets fired from the ship"""
+
+  def __init__(self, ai_game):
+    """Create a bullet object at the ship's current position."""
+    super().__init__()
+    self.screen = ai_game.screen
+    self.settings = ai_game.settings
+    self.color = self.settings.bullet_color
+
+    # Create a bullet rect at (0,0) then set the correct position
+    self.rect = pygame.Rect(0, 0, self.settings.bullet_width,
+        self.settings.bullet_height)
+    self.rect.midtop = ai_game.ship.rect.midtop
+
+    # Store the bullet's posiiton as a decimal value.
+    self.y = float(self.rect.y)

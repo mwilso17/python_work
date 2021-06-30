56ac50bea6d6172110618a64dbd911b1286b00d4
Mike Wilson <mwilso17@my.westga.edu>
2021-06-30 08:04:05 -0400

    added 2 methods to bullet

--- alien_invasion/bullet.py
+++ alien_invasion/bullet.py
@@ -21,3 +21,14 @@ class Bullet(Sprite):
 
     # Store the bullet's posiiton as a decimal value.
     self.y = float(self.rect.y)
+
+  def update(self):
+    """Move the bullet up the screen."""
+    # Update the decimal position of the bullet.
+    self.y -= self.settings.bullet_speed
+    # Update teh rect position.
+    self.rect.y = self.y
+
+  def draw_bullet(self):
+    """Draw the bullet to the screen."""
+    pygame.draw.rect(self.screen, self.color, self.rect)

2d07c513554ce030d0bd5abb85308352ee643101
Mike Wilson <mwilso17@my.westga.edu>
2021-07-04 12:20:03 -0400

    display 'lives' remaining. finish project for now.

--- alien_invasion/alien_invasion.py
+++ alien_invasion/alien_invasion.py
@@ -80,6 +80,7 @@ class AlienInvasion:
       self.stats.game_active = True
       self.sb.prep_score()
       self.sb.prep_level()
+      self.sb.prep_ships()
 
       # Get rid of any remaining aliens or bullets.
       self.aliens.empty()
@@ -196,8 +197,9 @@ class AlienInvasion:
   def _ship_hit(self):
     """Respond to ship being hit by an alien."""
     if self.stats.ships_left > 0:
-      # Decrement ships_left.
+      # Decrement ships_left, and update score board.
       self.stats.ships_left -= 1
+      self.sb.prep_ships()
 
       # Get rid of any remaining aliens and bullets.
       self.aliens.empty()

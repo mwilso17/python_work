fe8ac82f783b1d9da57a32ec8bf45801e5942b3d
Mike Wilson <mwilso17@my.westga.edu>
2021-07-08 06:05:54 -0400

    plotted point

--- matplotlib/scatter_squares.py
+++ matplotlib/scatter_squares.py
@@ -1,3 +1,10 @@
 # Mike Wilson 8 July 2021
 # Test of different parts of matplotlib.
 
+import matplotlib.pyplot as plt
+
+plt.style.use('seaborn')
+fig, ax = plt.subplots()
+ax.scatter(2, 4)
+
+plt.show()

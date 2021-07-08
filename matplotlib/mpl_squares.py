abbdecffb3eeca3f367e524b151804e001073fde
Mike Wilson <mwilso17@my.westga.edu>
2021-07-08 05:51:46 -0400

    code for a simple graph

--- matplotlib/mpl_squares.py
+++ matplotlib/mpl_squares.py
@@ -1,2 +1,11 @@
 # Mike Wilson 8 July 2021
 # A simple test of Matplotlib
+
+import matplotlib.pyplot as plt
+
+squares = [1, 4, 9, 16, 25]
+
+fig, ax = plt.subplots()
+ax.plot(squares)
+
+plt.show()

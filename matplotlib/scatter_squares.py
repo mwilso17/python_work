fd5dab73645d7349b574b95cbd307530a105c02d
Mike Wilson <mwilso17@my.westga.edu>
2021-07-08 06:15:32 -0400

    squares of all numbers, 1-1000

--- matplotlib/scatter_squares.py
+++ matplotlib/scatter_squares.py
@@ -3,12 +3,12 @@
 
 import matplotlib.pyplot as plt
 
-x_values = [1, 2, 3, 4 ,5]
-y_values = [1, 4, 9, 16, 25]
+x_values = range(1, 1001)
+y_values = [x**2 for x in x_values]
 
 plt.style.use('seaborn')
 fig, ax = plt.subplots()
-ax.scatter(x_values, y_values, s=200)
+ax.scatter(x_values, y_values, s=10)
 
 # Set chart title and label axes.
 ax.set_title("Square Numbers", fontsize=24)
@@ -17,5 +17,8 @@ ax.set_ylabel("Square of Value", fontsize=14)
 
 # Set size of tick labels.
 ax.tick_params(axis='both', which='major', labelsize=14)
+
+# Set range for each axis
+ax.axis([0, 1100, 0, 1100000])
 
 plt.show()

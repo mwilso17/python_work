07271ed54a7cb0f8b76c0ff5cffc0f6af9c3b6d9
Mike Wilson <mwilso17@my.westga.edu>
2021-07-09 08:21:34 -0400

    visualize rolling d6 1000 times

--- matplotlib/die_visual.py
+++ matplotlib/die_visual.py
@@ -1,6 +1,9 @@
 # Mike Wilson 9 July 2021
 # A visualization of the die class.
 
+from plotly.graph_objs import Bar, Layout
+from plotly import offline
+
 from die import Die
 
 # Create a D6.
@@ -18,4 +21,12 @@ for value in range(1, die.num_sides+1):
   frequency = results.count(value)
   frequencies.append(frequency)
 
-print(frequencies)
+# Visualize the results
+x_values = list(range(1, die.num_sides+1))
+data = [Bar(x=x_values, y=frequencies)]
+
+x_axis_config = {'title': 'Result'}
+y_axis_config = {'title': 'Frequency of Result'}
+my_layout = Layout(title="Results of rolling one D6 1000 times",
+  xaxis=x_axis_config, yaxis=y_axis_config)
+offline.plot({'data': data, 'layout': my_layout}, filename='d6.html')

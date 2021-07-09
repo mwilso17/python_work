d650f6d909a2513054a462abf56e0085ab85f729
Mike Wilson <mwilso17@my.westga.edu>
2021-07-09 07:07:24 -0400

    while loop & color fade

--- matplotlib/rw_visual.py
+++ matplotlib/rw_visual.py
@@ -1,15 +1,24 @@
 # Mike Wilson 8 July 2021
 # plot points on a random walk.
 
+from os import name
 import matplotlib.pyplot as plt
 from random_walk import RandomWalk
 
-# Make a random walk.
-rw = RandomWalk()
-rw.fill_walk()
+# Keep making new walks, as long as the program is active.
+while True:
+  # Make a random walk.
+  rw = RandomWalk()
+  rw.fill_walk()
 
-# Plot the points in a walk
-plt.style.use('classic')
-fig, ax = plt.subplots()
-ax.scatter(rw.x_values, rw.y_values, s=15)
-plt.show()
+  # Plot the points in a walk
+  plt.style.use('classic')
+  fig, ax = plt.subplots()
+  point_numbers = range(rw.num_points)
+  ax.scatter(rw.x_values, rw.y_values, c=point_numbers, cmap=plt.cm.Blues, 
+    edgecolors='none', s=15)
+  plt.show()
+
+  keep_running = input("Make another walk? (y/n): ")
+  if keep_running == 'n':
+    break

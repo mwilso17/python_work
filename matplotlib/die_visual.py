12e2edde3942e9b529c0ed35920c3fabf17afb1b
Mike Wilson <mwilso17@my.westga.edu>
2021-07-09 08:27:30 -0400

    roll d6 and d10 50k times and visualize

--- matplotlib/die_visual.py
+++ matplotlib/die_visual.py
@@ -8,11 +8,11 @@ from die import Die
 
 # Create two D6 dice.
 die_1 = Die()
-die_2 = Die()
+die_2 = Die(10)
 
 # Make some rolls and store the results in a list.
 results = []
-for roll_num in range(1000):
+for roll_num in range(50_000):
   result = die_1.roll() + die_2.roll()
   results.append(result)
 
@@ -29,6 +29,6 @@ data = [Bar(x=x_values, y=frequencies)]
 
 x_axis_config = {'title': 'Result', 'dtick': 1}
 y_axis_config = {'title': 'Frequency of Result'}
-my_layout = Layout(title="Results of rolling two D6 1000 times",
+my_layout = Layout(title="Results of rolling a d6 and d10 50000 times",
   xaxis=x_axis_config, yaxis=y_axis_config)
-offline.plot({'data': data, 'layout': my_layout}, filename='d6_d6.html')
+offline.plot({'data': data, 'layout': my_layout}, filename='d6_d10.html')

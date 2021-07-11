6c24333f90ddefd91e2d799d22023e30a8ac56e8
Mike Wilson <mwilso17@my.westga.edu>
2021-07-11 07:27:03 -0400

    initial code for sitka weather project

---
+++ matplotlib/sitka_highs.py
@@ -0,0 +1,15 @@
+# Mike Wilson 11 Jule 2021
+# Read data from data folder.
+
+import csv
+
+filename = 'matplotlib\data\sitka_weather_07-2018_simple.csv'
+with open(filename) as f:
+  reader = csv.reader(f)
+  header_row = next(reader)
+  print(header_row)
+
+  for index, column_header in enumerate(header_row):
+    print(index, column_header)
+
+  

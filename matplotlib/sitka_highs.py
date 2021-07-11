94248fcab744c68410e6c1934279a49aaa943d55
Mike Wilson <mwilso17@my.westga.edu>
2021-07-11 07:30:38 -0400

    removed header row

--- matplotlib/sitka_highs.py
+++ matplotlib/sitka_highs.py
@@ -7,7 +7,6 @@ filename = 'matplotlib\data\sitka_weather_07-2018_simple.csv'
 with open(filename) as f:
   reader = csv.reader(f)
   header_row = next(reader)
-  print(header_row)
 
   for index, column_header in enumerate(header_row):
     print(index, column_header)

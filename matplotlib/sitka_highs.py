5aa9bb7d95532a1dafdfed4b5d5e699940be4cf6
Mike Wilson <mwilso17@my.westga.edu>
2021-07-11 07:41:05 -0400

    created graph for tem highs

--- matplotlib/sitka_highs.py
+++ matplotlib/sitka_highs.py
@@ -3,6 +3,8 @@
 
 import csv
 
+import matplotlib.pyplot as plt
+
 filename = 'matplotlib\data\sitka_weather_07-2018_simple.csv'
 with open(filename) as f:
   reader = csv.reader(f)
@@ -14,6 +16,17 @@ with open(filename) as f:
     high = int(row[5])
     highs.append(high)
 
-print(highs)
+  # Plot the high temps.
+  plt.style.use('seaborn')
+  fig, ax = plt.subplots()
+  ax.plot(highs, c='red')
+
+  # Format plot.
+  ax.set_title("Daily high temperatures, July 2018", fontsize=24)
+  ax.set_xlabel('', fontsize=16)
+  ax.set_ylabel("Temperature (F)", fontsize=16)
+  ax.tick_params(axis='both', which='major', labelsize=16)
+
+plt.show()
 
   

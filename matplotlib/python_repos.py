b03273eeb8d4726e7b2045448f4f391537fc6368
Mike Wilson <mwilso17@my.westga.edu>
2021-07-13 05:51:40 -0400

    visualization

--- matplotlib/python_repos.py
+++ matplotlib/python_repos.py
@@ -3,29 +3,35 @@
 
 import requests
 
+from plotly.graph_objs import Bar 
+from plotly import offline
+
 # Make an API call and store the responses.
-url = 'https://api.github.com/search/repositories?q=language:python&sort=stats'
+url = 'https://api.github.com/search/repositories?q=language:python&sort=stars'
 headers = {'Accept': 'application/vnd.github.v3+json'}
 r = requests.get(url, headers=headers)
 print(f"Status code: {r.status_code}")
 
-# Store API response in a variable.
+# Process results.
 response_dict = r.json()
-print(f"Total repositiories: {response_dict['total_count']}")
-
-# Explore information about the repositories.
 repo_dicts = response_dict['items']
-print(f"Repositories returned: {len(repo_dicts)}")
-
-# Examine the first repository.
-repo_dict = repo_dicts[0]
-
-print("\nSelected information about each repository:")
+repo_names, stars = [], []
 for repo_dict in repo_dicts:
-  print(f"Name: {repo_dict['name']}")
-  print(f"Owner: {repo_dict['owner']['login']}")
-  print(f"Stars: {repo_dict['stargazers_count']}")
-  print(f"Repository: {repo_dict['html_url']}")
-  print(f"Created: {repo_dict['created_at']}")
-  print(f"Updated: {repo_dict['updated_at']}")
-  print(f"Description: {repo_dict['description']}")
+  repo_names.append(repo_dict['name'])
+  stars.append(repo_dict['stargazers_count'])
+
+# Make visulization
+data = [{
+  'type': 'bar',
+  'x': repo_names,
+  'y': stars,
+}]
+
+my_layout = {
+  'title': 'Most-Starred Python Projects on GitHub',
+  'xaxis': {'title': 'Repository'},
+  'yaxis': {'title': 'Stars'}
+}
+
+fig = {'data': data, 'layout': my_layout}
+offline.plot(fig, filename='python_repos.html')
